881e29e77b26745e984ce9918e9b3d8ee929f221
KalikaDakshini <kalikadakshini@gmail.com>
2025-05-06 11:40:59 +0530

    Added Day 19

---
+++ 19 - Etch A Sketch/main.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+if __name__ == "__main__":
+    talia = t.Turtle()
+    scilia = t.Screen()
+
+    talia.shape("turtle")
+    talia.pensize(3)
+    talia.color("red")
+
+    scilia.onkeypress(lambda: talia.left(10), "Left")
+    scilia.onkeypress(lambda: talia.right(10), "Right")
+    scilia.onkeypress(lambda: talia.fd(10), "Up")
+    scilia.onkeypress(lambda: talia.back(10), "Down")
+    scilia.onkeypress(
+        lambda: (talia.ht() if talia.isvisible() else talia.st()), "h"
+    )
+    scilia.listen()
+    scilia.exitonclick()
